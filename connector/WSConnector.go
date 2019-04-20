dcda203e2507bd164ac45dcf562f86919964a1ed
adoontheway <adoontheway@gmail.com>
2019-04-20 09:36:50 +0700

    wsconnector test connection pass

--- connector/WSConnector.go
+++ connector/WSConnector.go
@@ -1,6 +1,7 @@
 package connector
 
 import (
+	"bytes"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
@@ -11,9 +12,20 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:   1024,
 	WriteBufferSize:  1024,
 	HandshakeTimeout: time.Duration(3) * time.Second,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
-const MaxMessageSize int64 = 1024
+const (
+	maxMessageSize int64 = 1024
+	pongWait             = 60 * time.Second
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
 
 type wsConnector struct {
 	addr string
@@ -28,18 +40,39 @@ func NewWsconnector(addr string) IConnector {
 func serveWS(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 
-	defer func() {
-		conn.Close()
-	}()
-
-	conn.SetReadLimit(MaxMessageSize)
-
 	if err != nil {
 		log.Fatalf("Ws serve errror:%s", err)
 		return
 	}
 	//todo init session or not, need receive login protocol
 	//todo add channel to sub&pub&broadcast
+	go startReadLoop(conn)
+	go startWriteLoop(conn)
+}
+
+func startReadLoop(conn *websocket.Conn) {
+	defer func() {
+		conn.Close()
+	}()
+
+	conn.SetReadLimit(maxMessageSize)
+	conn.SetReadDeadline(time.Now().Add(pongWait))
+	//conn.PongHandler(func(string) error {conn.SetReadDeadline(time.Now().Add(pongWait));	return nil})
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error:%v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		log.Printf("Recieved message:%s", message)
+	}
+}
+
+func startWriteLoop(conn *websocket.Conn) {
+
 }
 
 func (wc *wsConnector) Start() error {
